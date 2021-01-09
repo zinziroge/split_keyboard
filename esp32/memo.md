4da79dfeea0c9afe56a9a0e0773fd1cf0cbef494
shin <zinziroge@gmail.com>
2021-01-09 23:41:14 +0900

    modify

---
+++ esp32/memo.md
@@ -0,0 +1,36 @@
+# BleKeyboard.cpp:
+
+* const 型　変数 PROGMEM = hoge; でFlashメモリを使う
+* extern const uint8_t _asciimap[128] PROGMEM;
+  * const uint8_t _asciimap[128] がflashメモリ
+  * これはacsiiコードからblekeyboardのkeycodeに変換するテーブル(=keycode)。
+    * 0x04~0x1d: a~z
+    * 0x84~0x9d: A-Z
+  * shift時の対応がUSキーボード配列になっている。例：
+   0x34|SHIFT,    // "
+   0x34,          // '
+  * なのでJPキーボード対応の1つの方法は _asciimap[] を書き換える
+* size_t BleKeyboard::press(uint8_t k)
+  * 0 <= k < 128: kは asciiコード として扱う。_asciimap[]でテーブル引きしてkeycodeに変換する
+    * keycode & 0x80 がtrueのときはshiftが押されているので、left shift modifierをつける
+  * 128 <= k < 144
+    * it's a modifier key
+      * 0x80-0x87: ctrl, shift, alt, gui(=windows key), 
+  * 144 <= k < 160
+    * international key
+  * 160 <= k < 256
+    * 136を引くので, 0x18~0x78
+    * it's a non-printing key (not a modifier)
+      * 0xB0-0xB3: Return/ESC/BS/Tab
+      * 0xC1:      Capslock
+      * 0xC2-0xCD: F1~F12 keys
+      * 0xD1-0xDA: Insert/Home/PageUp/Delete/End/PageDown/arrow keys
+      * 0xF0-0xFB: F13~F24 keys
+
+* HID Keyboard/Keypad Usage
+  * https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf
+  * https://beta.docs.qmk.fm/using-qmk/simple-keycodes/keycodes_basic
+
+* keymap -t1-> ascii -t2-> hid keycode
+  * t2は code + shift modifier
+  * 
--- esp32/split_kb.ino
+++ esp32/split_kb.ino
@@ -24,14 +24,247 @@
 #define MCP23017_GPPU_B_ADR      0x0D
 #define MCP23017_GPIO_B_ADR      0x13
 
-// 
+#define DOES_USE_I2C            0
+#define IS_ESP32                0
+#define IS_M5_STACK_CORE        1
+#define IS_ATOM_ECHO            2
+#define BOARD   IS_ESP32
+
+// ascii code -> blekey code(US)
+// 順番はasciiコード順、コードは
+// https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf の keyboard/keypad のコード
+#if 0
+#define SHIFT 0x80
+const uint8_t _asciimap[128] =
+{
+	0x00,             // NUL
+	0x00,             // SOH
+	0x00,             // STX
+	0x00,             // ETX
+	0x00,             // EOT
+	0x00,             // ENQ
+	0x00,             // ACK
+	0x00,             // BEL
+	0x2a,			// BS	Backspace
+	0x2b,			// TAB	Tab
+	0x28,			// LF	Enter
+	0x00,             // VT
+	0x00,             // FF
+	0x00,             // CR
+	0x00,             // SO
+	0x00,             // SI
+	0x00,             // DEL
+	0x00,             // DC1
+	0x00,             // DC2
+	0x00,             // DC3
+	0x00,             // DC4
+	0x00,             // NAK
+	0x00,             // SYN
+	0x00,             // ETB
+	0x00,             // CAN
+	0x00,             // EM
+	0x00,             // SUB
+	0x00,             // ESC
+	0x00,             // FS
+	0x00,             // GS
+	0x00,             // RS
+	0x00,             // US
+
+	0x2c,		   //  ' '
+	0x1e|SHIFT,	   // ! (1)
+	0x1f|SHIFT,    // " (2), 0x34|SHIFT
+	0x20|SHIFT,    // # (3)
+	0x21|SHIFT,    // $ (4)
+	0x22|SHIFT,    // % (5)
+	0x23|SHIFT,    // & (6), 0x24|SHIFT
+	0x24|SHIFT,    // ' (7), 0x34
+	0x25|SHIFT,    // ( (8), 0x26|SHIFT
+	0x26|SHIFT,    // ) (9), 0x27|SHIFT
+	0x25|SHIFT,    // *
+	0x33|SHIFT,    // +
+	0x36,          // ,
+	0x2d,          // -
+	0x37,          // .
+	0x38,          // /
+	0x27,          // 0
+	0x1e,          // 1
+	0x1f,          // 2
+	0x20,          // 3
+	0x21,          // 4
+	0x22,          // 5
+	0x23,          // 6
+	0x24,          // 7
+	0x25,          // 8
+	0x26,          // 9
+	0x33|SHIFT,      // :
+	0x33,          // ;
+	0x36|SHIFT,      // <
+	0x2e,          // =
+	0x37|SHIFT,     // >(.), 0x37|SHIFT
+	0x38|SHIFT,      // ?
+	0x1f|SHIFT,      // @ (2)
+	0x04|SHIFT,      // A
+	0x05|SHIFT,      // B
+	0x06|SHIFT,      // C
+	0x07|SHIFT,      // D
+	0x08|SHIFT,      // E
+	0x09|SHIFT,      // F
+	0x0a|SHIFT,      // G
+	0x0b|SHIFT,      // H
+	0x0c|SHIFT,      // I
+	0x0d|SHIFT,      // J
+	0x0e|SHIFT,      // K
+	0x0f|SHIFT,      // L
+	0x10|SHIFT,      // M
+	0x11|SHIFT,      // N
+	0x12|SHIFT,      // O
+	0x13|SHIFT,      // P
+	0x14|SHIFT,      // Q
+	0x15|SHIFT,      // R
+	0x16|SHIFT,      // S
+	0x17|SHIFT,      // T
+	0x18|SHIFT,      // U
+	0x19|SHIFT,      // V
+	0x1a|SHIFT,      // W
+	0x1b|SHIFT,      // X
+	0x1c|SHIFT,      // Y
+	0x1d|SHIFT,      // Z
+	0x2f,          // [
+	0x31,          // bslash
+	0x30,          // ]
+	0x23|SHIFT,    // ^
+	0x2d|SHIFT,    // _
+	0x35,          // `
+	0x04,          // a
+	0x05,          // b
+	0x06,          // c
+	0x07,          // d
+	0x08,          // e
+	0x09,          // f
+	0x0a,          // g
+	0x0b,          // h
+	0x0c,          // i
+	0x0d,          // j
+	0x0e,          // k
+	0x0f,          // l
+	0x10,          // m
+	0x11,          // n
+	0x12,          // o
+	0x13,          // p
+	0x14,          // q
+	0x15,          // r
+	0x16,          // s
+	0x17,          // t
+	0x18,          // u
+	0x19,          // v
+	0x1a,          // w
+	0x1b,          // x
+	0x1c,          // y
+	0x1d,          // z
+	0x2f|SHIFT,    // {
+	0x31|SHIFT,    // |
+	0x30|SHIFT,    // }
+	0x35|SHIFT,    // ~
+	0				// DEL
+};
+#else
+extern const uint8_t _asciimap[128];
+#endif
+
+#if 0 
+class BleKeyboardJP : public BleKeyboard
+{
+public:
+  size_t press(uint8_t k);
+  size_t write(uint8_t c);
+  size_t release(uint8_t k);
+};
+
+size_t BleKeyboardJP::press(uint8_t k)
+{
+	uint8_t i;
+	if (k >= 160) {			// 0xA0, it's a non-printing key (not a modifier)
+		k = k - 136;
+	} else if (k >= 144) { // 0x90, int1~9
+		k = k - 9;
+	} else if (k >= 128) {	// 0x80, it's a modifier key
+		_keyReport.modifiers |= (1<<(k-128));
+		k = 0;
+	} else {				// k < 0x80, it's a printing key
+		k = pgm_read_byte(_asciimap + k);
+		if (!k) {
+			setWriteError();
+			return 0;
+		}
+		if (k & 0x80) {						// it's a capital letter or other character reached with shift
+			_keyReport.modifiers |= 0x02;	// the left shift modifier
+			k &= 0x7F;
+		}
+	}
+
+	// Add k to the key report only if it's not already present
+	// and if there is an empty slot.
+	if (_keyReport.keys[0] != k && _keyReport.keys[1] != k &&
+		_keyReport.keys[2] != k && _keyReport.keys[3] != k &&
+		_keyReport.keys[4] != k && _keyReport.keys[5] != k) {
+
+		for (i=0; i<6; i++) {
+			if (_keyReport.keys[i] == 0x00) {
+				_keyReport.keys[i] = k;
+				break;
+			}
+		}
+		if (i == 6) {
+			setWriteError();
+			return 0;
+		}
+	}
+	sendReport(&_keyReport);
+	return 1;
+}
+
+size_t BleKeyboardJP::release(uint8_t k)
+{
+	uint8_t i;
+	if (k >= 160) {			// it's a non-printing key (not a modifier)
+		k = k - 136;
+	} else if (k >= 144) { // int1~9
+		k = k - 9;
+	} else if (k >= 128) {	// it's a modifier key
+		_keyReport.modifiers &= ~(1<<(k-128));
+		k = 0;
+	} else {				// it's a printing key
+		k = pgm_read_byte(_asciimap + k);
+		if (!k) {
+			return 0;
+		}
+		if (k & 0x80) {							// it's a capital letter or other character reached with shift
+			_keyReport.modifiers &= ~(0x02);	// the left shift modifier
+			k &= 0x7F;
+		}
+	}
+
+	// Test the key report to see if k is present.  Clear it if it exists.
+	// Check all positions in case the key is present more than once (which it shouldn't be)
+	for (i=0; i<6; i++) {
+		if (0 != k && _keyReport.keys[i] == k) {
+			_keyReport.keys[i] = 0x00;
+		}
+	}
+
+	sendReport(&_keyReport);
+	return 1;
+}
+//
+#endif
+
 BleKeyboard bleKeyboard("skb81_trainee");
 extern int col_pins[];
 extern int row_pins[];
 extern const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS];
 char buf[128];
 
-//http://kats-eye.net/info/2018/11/07/mcp23017-esp/
+ //http://kats-eye.net/info/2018/11/07/mcp23017-esp/
 void i2c_write_reg(byte dvc_adrs, uint8_t reg, uint8_t value)
 {
     Serial.println("ic2_write_reg()");
@@ -120,18 +353,22 @@ void setup(void)
     Serial.println(bleKeyboard.isConnected());
     //bleKeyboard.releaseAll();
 
+#if DOSE_USE_I2C
+#if BOARD == IS_ESP32
     // I2C
     //Wire.begin();//Wire.begin(SDA, SCL)
     //https://asukiaaa.blogspot.com/2020/03/arduinoi2c.html
     // esp32
-    //Wire.begin(14, 13);//Wire.begin(SDA, SCL)
-    //pinMode(13, INPUT);
-    //pinMode(14, INPUT);
+    Wire.begin(14, 13);//Wire.begin(SDA, SCL)
+    pinMode(13, INPUT);
+    pinMode(14, INPUT);
+#elif BOARD == IS_ATOM_ECHO
     // pico, M5 Atom Echo
     //https://wiki.seeedstudio.com/Grove_System/
     Wire.begin(26, 32);//Wire.begin(SDA, SCL), yello=SCL, white=SDA
     pinMode(26, INPUT);
     pinMode(32, INPUT);
+#elif BOARD == IS_M5_STACK_CORE
     //m5 stack core
     //Wire.begin(21, 22);//Wire.begin(SDA, SCL)
     //pinMode(21, INPUT);
@@ -141,6 +378,8 @@ void setup(void)
     i2c_write_reg(MCP23017_ADDR, MCP23017_GPPU_A_ADR, 0xFF);     // DVC00 I/O-PortA 入力ﾌﾟﾙｱｯﾌﾟ設定
     i2c_write_reg(MCP23017_ADDR, MCP23017_IODIR_B_ADR, 0x00);     // DVC00 I/O-PortB 出力設定, col
     i2c_write_reg(MCP23017_ADDR, MCP23017_GPIO_B_ADR, 0x00);     // DVC00 I/O-PortB 全OFF
+#endif /* BOARD */
+#endif /* DOSE_USE_I2C */
 }
 
 bool is_ascii(const uint16_t keycode)
@@ -162,7 +401,33 @@ bool is_modifier(const uint16_t keycode)
     return false;
 }
 
-void loop_1(void)
+void loop(void)
+{
+    //loop_test_keycode();
+    loop_paralle();
+    //loop_I2C_read();
+    //loop_find_I2C_device();
+    //loop_test_I2C();
+}
+
+void loop_test_keycode(void)
+{
+    int i;
+    if (bleKeyboard.isConnected()) {
+        for(i=0x20; i <= 0x7e; i++) {
+            sprintf(buf, "%d, '%c', 0x%2x",
+                i, i, _asciimap[i]);
+            Serial.println(buf);
+            bleKeyboard.write(_asciimap[i]); // keycode < 128 はascii code, それ以上は特別対応される
+            delay(1000);
+        }
+        while(1) {
+          delay(3000);
+        };
+    }
+}
+
+void loop_paralle(void)
 {
     static uint16_t prev_pressed_keycode = 0;
     static uint32_t prev_t = millis();
@@ -206,39 +471,7 @@ void loop_1(void)
     }
 }
 
-void loop_2(void) 
-{
-    static uint16_t prev_pressed_keycode = 0;
-    static uint32_t prev_t = millis();
-
-    if (1){
-        int c, r;
-        int sw_val;
-
-        for (c = 0; c < MATRIX_COLS; c++) { // output
-            i2c_digitalWrite(c, LOW);
-            for (r = 0; r < MATRIX_ROWS; r++) { // input
-                delay(KEY_SCAN_WAIT_MS);
-                uint32_t cur_t = millis();
-                sw_val = i2c_digitalRead(r);
-                uint16_t keycode = keymaps[0][r][c];
-                size_t ret = 0;
-
-                if (sw_val == 0) { // sw is pushed
-                    sprintf(buf, "(%d, %d)=%d,0x%02x,'%c', %d",
-                        r, c, sw_val, keycode, (char)(keycode & 0xFF), (int)ret);
-                    Serial.println(buf);
-                }
-            } // r
-            i2c_digitalWrite(c, HIGH);
-        } // c
-    }
-}
-
-void loop_3(void)
-{}
-
-void loop_4(void) 
+void loop_I2C_read(void) 
 {
     static uint16_t prev_pressed_keycode = 0;
     static uint32_t prev_t = millis();
@@ -268,7 +501,7 @@ void loop_4(void)
 }
 
 // find i2c device
-void loop_5()
+void loop_find_I2C_device()
 {
   byte error, address;
   int nDevices;
@@ -310,7 +543,7 @@ void loop_5()
   delay(5000);           // wait 5 seconds for next scan
 }
 
-void loop(void)
+void loop_test_I2C(void)
 {
     Serial.println("\nloop start");
 
